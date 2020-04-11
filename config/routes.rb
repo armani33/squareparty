0dee67d9aab5efea2d11d95ed1ef51afa9a13cbd
armand chariot <armand@chariot.be>
2020-04-11 15:00:20 +0200

    delete some space in routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-
   root to: 'pages#main'
   get '/dashboard', to: 'pages#dashboard'
 
@@ -8,4 +7,5 @@ Rails.application.routes.draw do
     get '/orders/:id/payment', to: 'orders#edit', as: 'payment_order'
     resources :orders
   end
+
 end
