2ba67a97d28df20c314beee81f45e9442a06d265
armand chariot <armand@chariot.be>
2020-04-11 21:37:30 +0200

    login backend done! nice work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authorized, only: [:main]
 
   def main
+    redirect_to dashboard_path(:h => current_user.product_id, :i => current_user.id) if logged_in?
+
     @products = Product.all.sort
     @referrer_code = params[:ref]
     @user_referrer = Order.find_by_referral_code(@referrer_code) if params[:ref]
@@ -12,6 +14,16 @@ class PagesController < ApplicationController
     @product = Product.find(params[:h])
     @order = Order.find(params[:i])
     @price = @order.product.price_in_cent.to_f / 100
+
+    unless session[:user_id] == @order.id
+      flash[:notice] = "You don't have access to that order!"
+      redirect_to root_path
+      # redirect_to dashboard_path(:h => @product, :i => @order)
+      # render :template => 'pages/dashboard'
+    end
+
   end
+
+
 
 end
