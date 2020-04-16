53c3bc0da1de68cef0a66e9d2352868fbd8ddc24
armand chariot <armand@chariot.be>
2020-04-16 13:17:44 +0200

    add slide_show image to prodcut show view + style

--- db/migrate/20200416111031_add_donation_amount_to_products.rb
+++ db/migrate/20200416111031_add_donation_amount_to_products.rb
@@ -1,4 +1,4 @@
-class AddDonationPriceToProducts < ActiveRecord::Migration[6.0]
+class AddDonationAmountToProducts < ActiveRecord::Migration[6.0]
   def change
     add_column :products, :donation_amount, :string
   end
