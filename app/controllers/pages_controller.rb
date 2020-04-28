92a43452827e72e1bee2922bd408f1d4440dbe71
armand chariot <armand@chariot.be>
2020-04-28 19:16:43 +0200

    main-mobile page style

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authorized, only: [:main]
+  before_action :set_variant
+  before_action :format
 
   def main
     redirect_to dashboard_path(:h => current_user.product_id, :i => current_user.id) if logged_in?
@@ -19,6 +21,33 @@ class PagesController < ApplicationController
       redirect_to root_path
       # redirect_to dashboard_path(:h => @product, :i => @order)
       # render :template => 'pages/dashboard'
+    end
+  end
+
+  private
+
+  def format
+    respond_to do |format|
+      format.html(&:phone)
+      # do |html|
+      # html.phone
+      # html.tablet
+      # end
+    end
+  end
+
+  def set_variant
+    case request.user_agent
+    when /iPhone/i
+      request.variant = :phone
+    when /Android/i && /mobile/i
+      request.variant = :phone
+    when /Windows Phone/i
+      request.variant = :phone
+    # when /Android/i
+    #   request.variant = :tablet
+    # when /iPad/i
+    #   request.variant = :tablet
     end
   end
 
